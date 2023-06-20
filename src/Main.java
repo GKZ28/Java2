dbb0bb4fde7eb420c77f076e9ef50f86e7079ae0
gkz <giorgadzekonstantin@gmail.com>
2023-06-20 15:15:58 +0400

    start3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        int
+        int x = 13_676;
+        int y = x / 20;
+        System.out.println("Количество бонусных миль:");
+        System.out.println(y);
     }
 }
